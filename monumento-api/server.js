726cd3325251a4bfdfb2ac528080684f802841f6
eliewebavenue <elie@webavenue.agency>
2025-08-18 11:27:20 +0200

    first commit

---
+++ monumento-api/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+//app.METHOD(CHEMIN, GESTIONNAIRE(req, res))
+
+app.get('/monuments/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`Vous avez demandé des informations sur le monument avec l'ID: ${id}`);
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
