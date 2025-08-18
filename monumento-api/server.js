bf742cb41501166acf927b7e318ee3c81cbb60c7
eliewebavenue <elie@webavenue.agency>
2025-08-18 16:34:48 +0200

    Réorganisation des fichiers : déplacement du modèle de données des monuments et mise à jour des chemins d'importation dans le serveur.

--- monumento-api/server.js
+++ monumento-api/server.js
@@ -1,61 +1,16 @@
 // Paqkage: monumento-api
 const express = require('express');
-let monuments = require('./mock-monument.js');
+let monuments = require('./src/db/mock-monument.js');
 const morgan = require('morgan');
 const favicon = require('serve-favicon');
 const { success } = require('./helper.js');
-const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('./src/db/sequelize.js');
 
 // Création de l'application Express
 const app = express(); 
 const port = 3000;
 
-//setup de la connexion à la base de données
-const sequelize = new Sequelize(
-  'monumento',
-  'root',
-  'root',
-  {
-    host: 'localhost',
-    port: 8889,
-    dialect: 'mysql',
-    dialectOptions: {
-      timezone: 'Etc/GMT+2',
-    },
-    logging: true,
-  } 
-)
-
-// Test de la connexion à la base de données
-sequelize
-  .authenticate()
-  .then(() => {  console.log('La connexion à la base de données a été établie avec succès.'); })
-  .catch(err => { console.error('Impossible de se connecter à la BDD:', err); });
-
-// Importation des models
-const MonumentModel = require('./src/models/monument.js')(sequelize, DataTypes);
-
-// Synchronisation des modèles avec la base de données
-sequelize.sync({alter: true})
-  .then(() => { 
-    
-    // console.log('Les modèles ont été synchronisés avec la base de données.'); 
-    // monuments.forEach(monument => {
-    //   MonumentModel.create({
-    //     id: monument.id,
-    //     title: monument.name,
-    //     country: monument.country,
-    //     city: monument.city,
-    //     buildYear: monument.buildYear,
-    //     picture: monument.picture,
-    //     description: monument.description,
-    //     created: monument.created
-    //   });
-    // });
-    // console.log('Les monuments ont été insérés dans la base de données.');
-
-  })
-  .catch(err => { console.error('Erreur lors de la synchronisation des modèles:', err); });
+sequelize.initializeDatabase();
 
 // Middlewares
 app
---
+++ monumento-api/src/db/sequelize.js
@@ -0,0 +1,32 @@
+const { Sequelize, DataTypes } = require('sequelize');
+let monuments = require('./mock-monument.js');
+
+//setup de la connexion à la base de données
+const sequelize = new Sequelize(
+  'monumento',
+  'root',
+  'root',
+  {
+    host: 'localhost',
+    port: 8889,
+    dialect: 'mysql',
+    dialectOptions: {
+      timezone: 'Etc/GMT+2',
+    },
+    logging: true,
+  } 
+)
+
+// Importation des models
+const MonumentModel = require('../models/monument.js')(sequelize, DataTypes);
+
+// Synchronisation des modèles avec la base de données
+const initializeDatabase = async () => {
+    return sequelize.sync({alter: true})
+  .then(() => { 
+    console.log('Les modèles ont été synchronisés avec la base de données.'); 
+  })
+  .catch(err => { console.error('Erreur lors de la synchronisation des modèles:', err); });
+}
+
+module.exports = { initializeDatabase, MonumentModel };
