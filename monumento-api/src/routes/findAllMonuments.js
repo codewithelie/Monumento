f8edd18fabfd34581459938dd227716c8a439e36
eliewebavenue <elie@webavenue.agency>
2025-08-19 14:41:57 +0200

    Ajout de la recherche de monuments par nom avec gestion des erreurs appropriée et mise à jour de la documentation des routes.

--- monumento-api/src/routes/findAllMonuments.js
+++ monumento-api/src/routes/findAllMonuments.js
@@ -1,15 +1,39 @@
 const { MonumentModel } = require('../db/sequelize.js');
+const { Op } = require('sequelize');
 
 module.exports = (app) => {
   app.get('/monuments', (req, res) => {
-    MonumentModel.findAll()
-      .then(monuments => {
-        const message = 'La liste des monuments a bien été récupérée.';
-        res.json({message, data: monuments});
+    if(req.query.name) {
+      const { name, limit, orderBy } = req.query;
+
+      MonumentModel.findAll({ 
+        where: {
+          name: {
+            [Op.like]: `%${name}%`
+          }
+        },
+        limit: limit ? parseInt(limit) : undefined,
+        order: orderBy ? [[orderBy, 'ASC']] : undefined
       })
-      .catch(error => {
-        const message = 'La liste des monuments n\'a pas pu être récupérée. Réessayez dans quelques instants.';
-        res.status(500).json({ message: message, data: error });
-      });
+        .then(monuments => {
+          if (monuments.length > 0) {
+            const message = `Les monuments avec le nom ${name} ont bien été trouvés.`;
+            res.json({ message, data: monuments });
+          } else {
+            const message = `Aucun monument trouvé avec le nom ${name}.`;
+            res.status(404).json({ message, data: null });
+          }
+        });
+    }else{
+      MonumentModel.findAll()
+        .then(monuments => {
+          const message = 'La liste des monuments a bien été récupérée.';
+          res.json({message, data: monuments});
+        })
+        .catch(error => {
+          const message = 'La liste des monuments n\'a pas pu être récupérée. Réessayez dans quelques instants.';
+          res.status(500).json({ message: message, data: error });
+        });
+    }
   });
 }
