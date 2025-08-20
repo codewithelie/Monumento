55199a7ece2220e6a861ba8f552f6efa1ce3fca8
eliewebavenue <elie@webavenue.agency>
2025-08-20 11:15:25 +0200

    Refactor de la fonction d'initialisation de la base de données pour créer un utilisateur par défaut et des monuments lors de la synchronisation des modèles.

--- monumento-api/src/db/sequelize.js
+++ monumento-api/src/db/sequelize.js
@@ -24,24 +24,51 @@ const UserModel = require('../models/user.js')(sequelize, DataTypes);
 
 // Synchronisation des modèles avec la base de données
 const initializeDatabase = async () => {
-    return sequelize.sync({alter: true})
-  .then(() => { 
-    console.log('Les modèles ont été synchronisés avec la base de données.'); 
-    // bcrypt.hash('admin', 10)
-    //   .then(hash => {
-    //     return UserModel.create({
-    //       username: 'admin',
-    //       password: hash,
-    //     });
-    //   })
-    //   .then(() => {
-    //     console.log('Utilisateur admin créé avec succès.');
-    //   })
-    //   .catch(err => {
-    //     console.error('Erreur lors de la création de l\'utilisateur admin:', err);
-    //   });
-  })
-  .catch(err => { console.error('Erreur lors de la synchronisation des modèles:', err); });
+  try { 
+    await sequelize.sync({ alter: true });
+    console.log('Les modèles ont été synchronisés avec la base de données.');
+
+    await createUser('admin', 'admin');
+
+    await createMonuments();
+
+  } catch (err) {
+    console.error('Erreur lors de la synchronisation des modèles:', err);
+  }
 }
+
+const createUser = async (username, password) => {
+  try {
+    const hash = await bcrypt.hash(password, 10);
+    const user = await UserModel.create({
+      username,
+      password: hash,
+    });
+    console.log(`Utilisateur ${username} créé avec succès.`);
+    return user;
+  } catch (err) {
+    console.error(`Erreur lors de la création de l'utilisateur ${username}:`, err);
+  }
+};
+
+const createMonuments = async () => {
+  try {
+    for (const monument of monuments) {
+      await MonumentModel.create({
+        id: monument.id,
+        title: monument.name,
+        country: monument.country,
+        city: monument.city,
+        buildYear: monument.buildYear,
+        picture: monument.picture,
+        description: monument.description,
+        created: monument.created,
+      });
+    }
+    console.log('Tous les monuments ont été créés avec succès.');
+  } catch (err) {
+    console.error('Erreur lors de la création des monuments:', err);
+  }
+};
 
 module.exports = { initializeDatabase, MonumentModel, UserModel };
